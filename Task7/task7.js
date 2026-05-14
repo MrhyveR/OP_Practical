ad9c109afd3b21fec7d9a62fd7153fe3bc36244f
MrhyveR <hirelingg2007@gmail.com>
2026-05-14 22:26:55 +0300

    add User, Logger, and NotificationService

--- Task7/task7.js
+++ Task7/task7.js
@@ -15,3 +15,52 @@ class EventBus extends EventEmitter {
 }
 
 const bus = new EventBus();
+
+class Logger {
+    constructor(bus) {
+        this.bus = bus;
+        this.logHandler = this.logHandler.bind(this);
+    }
+
+    startLogging() {
+        this.bus.subscribe('userAction', this.logHandler);
+        console.log('Logger: Підписався на події (Логування увімкнено).');
+    }
+
+    logHandler(data) {
+        console.log(`[LOG]: Користувач ${data.user} виконав дію: ${data.action}`);
+    }
+}
+
+class NotificationService {
+    constructor(bus) {
+        this.bus = bus;
+        this.notifyHandler = this.notifyHandler.bind(this);
+    }
+
+    enableNotifications() {
+        this.bus.subscribe('userAction', this.notifyHandler);
+        console.log('NotificationService: Підписався на події (Сповіщення увімкнено).');
+    }
+
+    disableNotifications() {
+        this.bus.unsubscribe('userAction', this.notifyHandler);
+        console.log('NotificationService: Відписався від подій (Сповіщення вимкнено).');
+    }
+
+    notifyHandler(data) {
+        console.log(`[ALERT]: Надсилання email. Користувач ${data.user} зробив: ${data.action}`);
+    }
+}
+
+class User {
+    constructor(name, bus) {
+        this.name = name;
+        this.bus = bus;
+    }
+
+    performAction(action) {
+        console.log(`\nІніціація: ${this.name} виконує "${action}" ---`);
+        this.bus.publish('userAction', { user: this.name, action: action });
+    }
+}
