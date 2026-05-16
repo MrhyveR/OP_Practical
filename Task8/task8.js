7e492af46cc81191ae94859da60961d219b50768
MrhyveR <hirelingg2007@gmail.com>
2026-05-16 09:27:47 +0300

    add dynamic authentication strategies and header injection

--- Task8/task8.js
+++ Task8/task8.js
@@ -1,12 +1,34 @@
 class AuthProxy {
     constructor(baseUrl) {
         this.baseUrl = baseUrl;
+        this.authStrategy = null;
+    }
+
+    setAuthStrategy(strategy) {
+        this.authStrategy = strategy;
+        console.log(`[Log] Auth strategy set to: ${strategy.type}`);
+    }
+
+    _injectAuthHeaders(headers = {}) {
+        if (!this.authStrategy) return headers;
+
+        const { type, credentials } = this.authStrategy;
+        const newHeaders = { ...headers };
+
+        if (type === 'API_KEY') {
+            newHeaders['X-Api-Key'] = credentials;
+        } else if (type === 'JWT' || type === 'OAuth') {
+            newHeaders['Authorization'] = `Bearer ${credentials}`;
+        }
+        
+        return newHeaders;
     }
 
     async request(endpoint, options = {}) {
         const url = `${this.baseUrl}${endpoint}`;
-        
         console.log(`[Log] Requesting: ${options.method || 'Get'} ${url}`);
+
+        options.headers = this._injectAuthHeaders(options.headers || {});
 
         try {
             const response = await fetch(url, options);
